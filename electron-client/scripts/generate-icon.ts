fafd06cc1d357589697a613d50186daaeae96fc8
Milo <tomilobas@gmail.com>
2024-10-04 22:54:31 +0200

    Script for generating icons

---
+++ electron-client/scripts/generate-icon.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import fs from "fs";
+import { exec } from "child_process";
+import util from "util";
+
+const execAsync = util.promisify(exec);
+
+(async function () {
+  const imagePath = path.join(__dirname, "../packaging/app_icon.png");
+  const iconOutputDir = path.join(__dirname, "../packaging/iconset");
+
+  await fs.promises.mkdir(iconOutputDir, { recursive: true });
+
+  const iconConfigurations = [
+    { size: 16, suffix: '' },
+    { size: 32, suffix: '@2x' },
+    { size: 32, suffix: '' },
+    { size: 64, suffix: '@2x' },
+    { size: 128, suffix: '' },
+    { size: 256, suffix: '@2x' },
+    { size: 256, suffix: '' },
+    { size: 512, suffix: '@2x' },
+    { size: 512, suffix: '' },
+    { size: 1024, suffix: '@2x' }
+  ];
+
+  for (const config of iconConfigurations) {
+    const outputPath = path.join(iconOutputDir, `icon_${config.size}x${config.size}${config.suffix}.png`);
+    await execAsync(`magick ${imagePath} -resize ${config.size}x${config.size} ${outputPath}`);
+  }
+})();
