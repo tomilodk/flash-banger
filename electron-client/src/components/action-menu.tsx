4338046d399ba7f2bc53fa25c96d7ffd9eae140f
Milo <tomilobas@gmail.com>
2024-10-06 23:29:34 +0200

    Command

--- electron-client/src/components/action-menu.tsx
+++ electron-client/src/components/action-menu.tsx
@@ -10,6 +10,10 @@ const ActionMenu: React.FC = () => {
         visible && (
             <Card id='action-menu' className='w-[350px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-3'>
                 {React.createElement(ACTIONS[action])}
+
+                <div className="flex justify-center items-center">
+                    <span className="mt-3 text-muted-foreground text-xs left-0 pb-0 w-full text-center">ALT + SHIFT + .</span>
+                </div>
             </Card>
         )
     );
