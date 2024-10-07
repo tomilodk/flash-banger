55ba716d32d1dcd188e13387fe6b9a1864ea2758
Milo <tomilobas@gmail.com>
2024-10-07 21:43:48 +0200

    v replace

--- electron-client/scripts/set-version.ts
+++ electron-client/scripts/set-version.ts
@@ -4,9 +4,9 @@ import path from 'path';
 const packageJsonPath = path.join(__dirname, '../package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-const newVersion = process.argv[2];
+const newVersion = process.argv[2].replace(/^v/, '');
 
-packageJson.version = newVersion.replace(/^v/, '');
+packageJson.version = newVersion,
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 fs.writeFileSync(path.join(__dirname, '../src/runtime-version.ts'), `export const runtimeVersion = "${newVersion}";`);
