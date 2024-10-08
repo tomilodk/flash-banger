4619dd40630ad19260c19aaf97ee59041b09d871
Milo <tomilobas@gmail.com>
2024-10-08 19:33:48 +0200

    Added back update-electron-app

--- electron-client/src/setup/auto-update.ts
+++ electron-client/src/setup/auto-update.ts
@@ -1,23 +1,15 @@
 import { logger } from '../lib/logger';
-import { autoUpdater } from 'electron';
+import { updateElectronApp, UpdateSourceType } from 'update-electron-app';
 
 export function addAutoUpdate() {
-    setInterval(() => {
-        logger.info('Checking for updates')
-        autoUpdater.checkForUpdates()
-    }, 60000)
-
-    autoUpdater.on('update-available', () => {
-        logger.info('Update available');
-    })
-
-    autoUpdater.on('update-downloaded', () => {
-        logger.info('Update downloaded, quitting and installing');
-        autoUpdater.quitAndInstall()
-    })
-
-    autoUpdater.on('error', (message) => {
-        logger.error('There was a problem updating the application')
-        logger.error(JSON.stringify(message))
-    })
+    updateElectronApp({
+        updateSource: {
+            type: UpdateSourceType.ElectronPublicUpdateService,
+            repo: 'tomilodk/flash-banger',
+            host: "https://github.com"
+        },
+        updateInterval: "5 minutes",
+        notifyUser: false,
+        logger: logger,
+    });
 }
