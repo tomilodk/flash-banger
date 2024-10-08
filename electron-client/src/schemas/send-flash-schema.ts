d2f3f5a1db4fb7dd1e2df6a84b185419f9a55d8e
Milo <tomilobas@gmail.com>
2024-10-08 19:22:30 +0200

    250 characters and questionmark

--- electron-client/src/schemas/send-flash-schema.ts
+++ electron-client/src/schemas/send-flash-schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const sendFlashSchema = z.object({
-    text: z.string().min(1).max(100).regex(/^[a-zA-Z0-9\s,.æøåÆØÅ]+$/, "Only letters, numbers, spaces, commas, and periods are allowed")
+    text: z.string().min(1).max(250).regex(/^[a-zA-Z0-9\s,.æøåÆØÅ?]+$/, "Only letters, numbers, spaces, commas, and periods are allowed")
 })
