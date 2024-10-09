cac9337d968694df4d86c3eecc6ebc92bb0e431e
Milo R. Jørgensen <developer@holion.dk>
2024-10-09 13:57:25 +0200

    Added emojies

--- electron-client/src/schemas/send-flash-schema.ts
+++ electron-client/src/schemas/send-flash-schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const sendFlashSchema = z.object({
-    text: z.string().min(1).max(250).regex(/^[a-zA-Z0-9\s,.æøåÆØÅ?]+$/, "Only letters, numbers, spaces, commas, and periods are allowed")
+    text: z.string().min(1).max(250).regex(/^[a-zA-Z0-9\s,.æøåÆØÅ?\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]]+$/, "Only letters, numbers, spaces, commas, and periods are allowed")
 })
